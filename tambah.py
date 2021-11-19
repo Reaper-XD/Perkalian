3032bc2cfd41d03e99c3f70b98a4882f9818860f
GRCR4K3R <94230332+GRCR4K3R@users.noreply.github.com>
2021-11-19 17:01:06 +0700

    Create tambah.py

---
+++ tambah.py
@@ -0,0 +1,47 @@
+#!/bin/py
+
+#module
+import os,sys,time,requests
+from time import sleep
+
+#tampilan
+os.system("clear")
+os.system("figlet kalkulator")
+tampilan ="""
+===========================================================
+=       (+)Author     : Reza Alfauzan                     =
+=       (+)Github     : https://github.com/GRCR4K3R       =
+=       (+)Youtube    : Reja Gaming                       =
+=       (+)Pesan      : Jangan Recode Mas:)               =
+=              (+)===  Welcome To Termux ===(+)           =
+==========================================================="""
+sleep(1)
+print(tampilan)
+print("")
+print("1)Pertambahan")
+print("2)Pengurangan")
+print("3)Perkalian")
+print("4)Pembagian")
+print("_______________________")
+pilih =input("Pilih Salah Satu")
+
+#Data Pertambahan
+if pilih =="1":
+        angka1 =int(input("Angka Pertama :"))
+        angka2 =int(input("Angka Kedua :"))
+        print(angka1 + angka2)
+#Data Pengurangan
+if pilih =="2":
+        angka1 =int(input("Angka Pertama :"))
+        angka2 =int(input("Angka Kedua : "))
+        print(angka1 - angka2)
+#Data Perkalian
+if pilih =="3":
+        angka1 =int(input("Angka Pertama :"))
+        angka2 =int(input("Angka Kedua :"))
+        print(angka1 * angka2)
+#Data Pembagian
+if pilih =="4":
+        angka1 =int(input("Angka Pertama :"))
+        angka2 =int(input("Angka Kedua :"))
+        print(angka1 / angka2)
